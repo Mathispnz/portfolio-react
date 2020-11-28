3039f73de88e166db9ab6950c37906e4d41a5837
Mathispnz <mathis.peingnez@gmail.com>
2020-11-28 10:57:16 +0100

    Updates

---
+++ precache-manifest.84ab9260424e20f4b09642d79a62106e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9e8f0ed481e3ed50f8d99ad1c4d48c9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c2a3b8027acf8ec98419",
+    "url": "/static/css/main.52ecd6d6.chunk.css"
+  },
+  {
+    "revision": "0092f5ba89f0c3210b80",
+    "url": "/static/js/2.3f27a1cf.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.3f27a1cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2a3b8027acf8ec98419",
+    "url": "/static/js/main.f8f1adb8.chunk.js"
+  },
+  {
+    "revision": "0934e5a0629121a12638",
+    "url": "/static/js/runtime-main.c542aba9.js"
+  }
+]);
